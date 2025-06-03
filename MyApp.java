7b0ee36e0ad0d75e3e858e424c209cedc059f9b4
Robiul <robiulhossain0110@gmail.com>
2025-06-03 12:46:24 +0600

    new java

---
+++ MyApp.java
@@ -0,0 +1,22 @@
+public class MyApp{
+
+ 
+     public static void main(String[] args) {
+        byte b = 127;
+        Byte b2 = 80;
+        Character c2 = 'A'; //wrapper class
+        Short s = 1000;
+        Long l = 123456789L;
+        Float f = 5.75f;
+        Double d = 19.99d;
+        Boolean bo = true;
+        System.out.println("Byte: "+ b2);
+        System.out.println("Character: "+ c2);
+        System.out.println("Short: "+ s);
+        System.out.println("Long: "+ l);
+        System.out.println("Float: "+ f);
+        System.out.println("Double: "+ d);
+        System.out.println("Boolean: "+ bo);
+        
+    }
+}
